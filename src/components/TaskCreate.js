4b79f38e94f3cbd319581434eda3953356563dfc
Philipp Tanlak <philipp.tanlak@gmail.com>
2019-03-03 13:36:51 +0100

    Add placeholders

--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,19 +1,30 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+const placeholders = [
+  'Layout my personal portfolio',
+  'Coffe break',
+  'Learn about this new framework',
+  'Read new articles on Hacker News',
+  'Structure the new platform achitecture',
+  'Design a new logo for my customer',
+  'Refactor code of my TodoMVC',
+  'Tell my friends about Timmi',
+  'Schedule meeting about new project idea',
+];
+const randomPlaceholder =
+  placeholders[Math.floor(Math.random() * placeholders.length)];
+
 export default function TaskCreate({ onSubmit }) {
   const [title, setTitle] = useState('');
   const [buttonText, setButtonText] = useState('Start Timer');
-  const [buttonActive, setButtonActive] = useState(false);
   const submit = () => {
     if (title === '') return;
     onSubmit(title);
     setTitle('');
     setButtonText('Timer started...');
-    setButtonActive(true);
     setTimeout(() => {
       setButtonText('Start timer');
-      setButtonActive(false);
     }, 2000);
   };
   const handleKeyDown = e => e.which === 13 && submit();
@@ -26,12 +37,9 @@ export default function TaskCreate({ onSubmit }) {
         value={title}
         onChange={e => setTitle(e.target.value)}
         onKeyDown={handleKeyDown}
+        placeholder={randomPlaceholder}
       />
-      <button
-        type="button"
-        className={`task-create-submit ${(title || buttonActive) && 'active'}`}
-        onClick={submit}
-      >
+      <button type="button" className="task-create-submit" onClick={submit}>
         {buttonText}
       </button>
     </div>
